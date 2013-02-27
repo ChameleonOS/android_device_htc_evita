ff5442d7b53915235e61c96ec2dcfcf5081f363f
0xD34D <clark@scheffsblend.com>
2013-02-27 08:13:42 -0800

    Converting to ChameleonOS for jellybean-mr1

--- cos.mk
+++ cos.mk
@@ -1,5 +1,5 @@
 # Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/cos/config/gsm.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := evita
@@ -8,15 +8,15 @@ PRODUCT_RELEASE_NAME := evita
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common COS stuff.
+$(call inherit-product, vendor/cos/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/htc/evita/device_evita.mk)
 
 # Device naming
 PRODUCT_DEVICE := evita
-PRODUCT_NAME := cm_evita
+PRODUCT_NAME := cos_evita
 PRODUCT_BRAND := htc
 PRODUCT_MODEL := One X
 PRODUCT_MANUFACTURER := HTC
